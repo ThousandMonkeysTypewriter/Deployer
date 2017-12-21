c002dec70cb9652567378ab81d3fc7b5835947d8
nayname <nayname@gmail.com>
2017-12-21 11:38:12 +0300

    Create Bicycle.java

---
+++ src/main/java/org/examples/Bicycle.java
@@ -0,0 +1,37 @@
+package org.examples;
+
+public class Bicycle {
+        
+    // the Bicycle class has
+    // three fields
+    public int cadence;
+    public int gear;
+    public int speed;
+        
+    // the Bicycle class has
+    // one constructor
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+        
+    // the Bicycle class has
+    // four methods
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+        
+}
